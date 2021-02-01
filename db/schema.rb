2153a331c2406372d106dd48d8ff3c07bf2de1a7
Hermyson <hermysoncassiano@gmail.com>
2021-02-01 01:58:21 -0300

    Criando tabela cliente

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_194533) do
+ActiveRecord::Schema.define(version: 2021_02_01_045356) do
 
   create_table "agendas", force: :cascade do |t|
     t.text "mensagem"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "clientes", force: :cascade do |t|
+    t.string "nome"
+    t.string "telefone"
+    t.string "celular"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
