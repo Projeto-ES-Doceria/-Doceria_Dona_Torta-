724c654eda03d8287fd1a10dd5f0ce7885bd0302
Hermyson <hermysoncassiano@gmail.com>
2021-02-01 23:15:49 -0300

    Validates models cliente e endereço

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,8 +2,9 @@ class Cliente < ApplicationRecord
   belongs_to :endereco
 
   validates_uniqueness_of :email
-  validates :nome, :telefone, :email, presence: true
+  validates :nome, :telefone,:celular,:email, presence: true
   validates_length_of :nome, minimum: 10
   validates_numericality_of :telefone
+  validates_numericality_of :celular
   validates_associated :endereco
 end
