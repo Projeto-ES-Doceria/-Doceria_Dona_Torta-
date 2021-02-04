2d4eb5edd251fefc205b4c608eaf01d90fa96289
Hermyson <hermysoncassiano@gmail.com>
2021-02-03 23:31:15 -0300

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_015955) do
+ActiveRecord::Schema.define(version: 2021_02_02_034947) do
 
   create_table "agendas", force: :cascade do |t|
     t.text "mensagem"
