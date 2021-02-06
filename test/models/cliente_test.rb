cb875ef6e8abadcd052147a3d0c33340c0595fb7
Hermyson <hermysoncassiano@gmail.com>
2021-02-06 00:48:14 -0300

    Criado testes Model cliente e endereço

--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class ClienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'deve salvar o cliente com nome, telefone, celular, email e endereco' do
+    endereco = Endereco.new  rua: 'rua numero1',numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+    endereco.save
+    cliente = Cliente.new nome: 'Leonardo Noronha', telefone: '376173333',celular:'999994444',email:'leonardo@gmail.com', endereco_id: endereco.id
+    assert cliente.save
+  end
+  test 'deve deletar o cliente' do
+    endereco = Endereco.new  rua: 'rua numero1',numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+    endereco.save
+    cliente = Cliente.new nome: 'Leonardo Noronha', telefone: '376173333',celular:'999994444',email:'leonardo@gmail.com', endereco_id: endereco.id
+    cliente.save
+    assert cliente.destroy
+  end
+  test 'deve editar o cliente de nome' do
+    endereco = Endereco.new  rua: 'rua numero1',numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+    endereco.save
+    cliente = Cliente.new nome: 'Leonardo Noronha', telefone: '376173333',celular:'999994444',email:'leonardo@gmail.com', endereco_id: endereco.id
+    cliente.save
+    assert cliente.update nome: 'Leonardo Noronha Silva'
+  end
+  test 'não deve salvar cliente sem rua' do
+    endereco = Endereco.new  numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+    endereco.save
+    cliente = Cliente.new nome: 'Leonardo Noronha', telefone: '376173333',celular:'999994444',email:'leonardo@gmail.com', endereco_id: endereco.id
+    assert_not cliente.save
+  end
+  test 'não deve salvar com nome violando complemento minimo' do
+    endereco = Endereco.new  rua: 'rua numero1',numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+    endereco.save
+    cliente = Cliente.new nome: 'Leonardo ', telefone: '376173333',celular:'999994444',email:'leonardo@gmail.com', endereco_id: endereco.id
+    assert_not cliente.save
+  end
+  test 'deve editar o cliente com formato do telefone diferente' do
+    endereco = Endereco.new  rua: 'rua numero1',numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+    endereco.save
+    cliente = Cliente.new nome: 'Leonardo Noronha', telefone: '376173333',celular:'999994444',email:'leonardo@gmail.com', endereco_id: endereco.id
+    cliente.save
+    assert_not cliente.update telefone: '37617AA33'
+  end
 end
--- test/models/endereco_test.rb
+++ test/models/endereco_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class EnderecoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'deve salvar endreco com rua, numero, bairro, cidade, cep, estado,complemento' do
+    endereco = Endereco.new  rua: 'rua numero1',numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+    assert endereco.save
+  end
+  test 'deve deletar o endereco' do
+  endereco = Endereco.new  rua: 'rua numero1',numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+  assert endereco.delete
+  end
+  test 'deve editar a rua' do
+    endereco = Endereco.new  rua: 'rua numero1',numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+    assert endereco.update rua: 'Paraiba'
+  end
+  test 'não deve salvar endreco sem rua'do
+    endereco = Endereco.new  numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'55290000',estado:'pe',complemento: 'prox hospital'
+    assert_not endereco.save
+  end
+  test 'não deve salvar endreco com cep formato violado'do
+    endereco = Endereco.new  numero: '202',bairro:'centro', cidade: 'Garanhuns',cep:'5529AAAAA',estado:'pe',complemento: 'prox hospital'
+    assert_not endereco.save
+  end
 end
