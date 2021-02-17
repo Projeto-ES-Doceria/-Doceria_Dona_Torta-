adf25fab89d6ff579129058bdb99dd038b41045e
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2021-02-17 09:34:16 -0300

    Adicionando scaffold de produto

---
+++ app/models/produto.rb
@@ -0,0 +1,8 @@
+class Produto < ApplicationRecord
+  validates :nome, presence: true, uniqueness: true, length: {min: 4, max: 20}
+  validates :descricao, presence: true
+  validates :fabricacao, presence: true
+  validates :vencimento, presence: true
+  validates :quantidade, numericality: {only_integer: true, greater_than: 0}
+  validates :preco, presence: true
+end
